b0b8ff1cc5cec349f86ce32a9b206721b8a294ba
zhangjt <zhangjt007@gmail.com>
2020-05-18 20:26:29 +0800

    route新增

--- src/cnap/CommonTools.py
+++ src/cnap/CommonTools.py
@@ -14,17 +14,69 @@ import pandas as pd
 cnap_excel_file = "../../resource/人行二代基础数据.xls"
 
 
-def msg_type_to_route_code(msg_type):
+def msg_type_to_route_code(msg_type, pay_channel):
+    """
+    根据报文类型转换为汇路标识
+    :param msg_type:
+    :return:
+    """
     payChannel = {
-        "hvps": "00",
-        "beps": "01",
-        "ccms": "02",
-        "saps": "03",
-        "nets": "04",
-        "pbcs": "05",
-        "ibps": "06"
+        "hvps": "0",
+        "beps": "1",
+        "ccms": "2",
+        "saps": "3",
+        "nets": "4",
+        "pbcs": "5",
+        "ibps": "6",
+        "maps": "7"
     }
-    return payChannel[msg_type[0:4]] + msg_type[4:].replace(".", "").replace("0", "")
+    return payChannel[pay_channel.lower()] + payChannel[msg_type[0:4]] + msg_type[5:8] + msg_type[9:].replace(".",
+                                                                                                              "").replace(
+        "0", "")
+
+
+msg_type_service_type_rel = {
+    "hvps.111.001": "961001",
+    "hvps.112.001": "961001",
+    "hvps.141.001": "963001",
+    "hvps.141.002": "963001",
+    "beps.121.001": "961001",
+    "beps.122.001": "961001",
+    "beps.123.001": "961001",
+    "beps.125.001": "961003",
+    "beps.127.001": "962001",
+    "beps.131.001": "962001",
+    "beps.133.001": "962003"
+}
+
+
+def msg_type_to_service_type(msg_type):
+    start_index = 963010
+    if msg_type[:12] in msg_type_service_type_rel:
+        service_type = msg_type_service_type_rel[msg_type[:12]]
+    else:
+        service_type = str(start_index + len(msg_type_service_type_rel))
+        msg_type_service_type_rel[msg_type[:12]] = service_type
+    return service_type
+
+
+def get_ibps_msg_type(is_only_req=False, is_only_resp=False):
+    """
+    解析HVPS报文清单作为基准数据。
+    :return:
+    """
+    ibps_msg_type = pd.read_excel(cnap_excel_file, sheet_name="IBPS报文清单", usecols=['报文编号', '报文名称', '报文方向'])
+    ibps_msg_type['通道编码'] = "IBPS"
+    if is_only_req:
+        print("只获取往账报文")
+        return ibps_msg_type[['报文编号', '报文名称', '通道编码']][
+            ibps_msg_type['报文方向'].str.contains('参与者->|参与者<->|参与机构<->|参与机构->')]
+    if is_only_resp:
+        print("只获取来账报文")
+        return ibps_msg_type[['报文编号', '报文名称', '通道编码']][ibps_msg_type['报文方向'].str.contains('参与者<|参与机构<|>参与者|>参与机构')]
+    print("获取全量报文")
+    ibps_msg_type['报文名称'] = ibps_msg_type['报文名称'].apply(lambda x: x.strip())
+    return ibps_msg_type[['报文编号', '报文名称', '通道编码']]
 
 
 def get_hvps_msg_type(is_only_req=False, is_only_resp=False):
@@ -42,6 +94,7 @@ def get_hvps_msg_type(is_only_req=False, is_only_resp=False):
         print("只获取来账报文")
         return hvps_msg_type[['报文编号', '报文名称', '通道编码']][hvps_msg_type['报文方向'].str.contains('参与者<|参与机构<|>参与者|>参与机构')]
     print("获取全量报文")
+    hvps_msg_type['报文名称'] = hvps_msg_type['报文名称'].apply(lambda x: x.strip())
     return hvps_msg_type[['报文编号', '报文名称', '通道编码']]
 
 
@@ -60,6 +113,7 @@ def get_beps_msg_type(is_only_req=False, is_only_resp=False):
         print("只获取来账报文")
         return beps_msg_type[['报文编号', '报文名称', '通道编码']][beps_msg_type['报文方向'].str.contains('参与者<|参与机构<|>参与者|>参与机构')]
     print("获取全量报文")
+    beps_msg_type['报文名称'] = beps_msg_type['报文名称'].apply(lambda x: x.lstrip())
     return beps_msg_type[['报文编号', '报文名称', '通道编码']]
 
 
@@ -78,6 +132,7 @@ def get_saps_msg_type(is_only_req=False, is_only_resp=False):
         print("只获取来账报文")
         return saps_msg_type[['报文编号', '报文名称', '通道编码']][saps_msg_type['报文方向'].str.contains('参与者<|参与机构<|>参与者|>参与机构')]
     print("获取全量报文")
+    saps_msg_type['报文名称'] = saps_msg_type['报文名称'].apply(lambda x: x.strip())
     return saps_msg_type[['报文编号', '报文名称', '通道编码']]
 
 
@@ -96,6 +151,7 @@ def get_ccms_msg_type(is_only_req=False, is_only_resp=False):
         print("只获取来账报文")
         return ccms_msg_type[['报文编号', '报文名称', '通道编码']][ccms_msg_type['报文方向'].str.contains('参与者<|参与机构<|>参与者|>参与机构')]
     print("获取全量报文")
+    ccms_msg_type['报文名称'] = ccms_msg_type['报文名称'].apply(lambda x: x.strip())
     return ccms_msg_type[['报文编号', '报文名称', '通道编码']]
 
 
@@ -108,5 +164,6 @@ def get_all_cnap_msg_type(is_only_req=False, is_only_resp=False):
     beps_msg_type = get_beps_msg_type(is_only_req, is_only_resp)
     saps_msg_type = get_saps_msg_type(is_only_req, is_only_resp)
     ccms_msg_type = get_ccms_msg_type(is_only_req, is_only_resp)
-    concat_result = pd.concat([hvps_msg_type, beps_msg_type, saps_msg_type, ccms_msg_type])
+    ibps_msg_type = get_ibps_msg_type(is_only_req, is_only_resp)
+    concat_result = pd.concat([hvps_msg_type, beps_msg_type, saps_msg_type, ccms_msg_type, ibps_msg_type])
     return concat_result
--- src/cnap/ServiceBusiType.py
+++ src/cnap/ServiceBusiType.py
@@ -9,6 +9,7 @@
 @License : (C)Copyright 2020-2021, SOLO
 @Des     : 获取汇路信息
 """
+import xml.etree.ElementTree as et
 
 from src.cnap.CommonTools import *
 
@@ -19,27 +20,18 @@ def generate_route_info(data):
     :param data:
     :return:
     """
+    data['PART_MSGTYPE'] = data['MSGTYPE'].apply(lambda x: x[:12])
+    data.drop_duplicates(['SYSCODE', 'PART_MSGTYPE'], keep="first", inplace=True)
+    all_cnap_msg_type = get_all_cnap_msg_type(is_only_req=True)
+    all_cnap_msg_type['PART_MSGTYPE'] = all_cnap_msg_type['报文编号'].apply(lambda x: str(x)[:12])
+    result = pd.merge(data, all_cnap_msg_type, on='PART_MSGTYPE', how='outer')
+    result.to_excel("../../resource/cnap/BUPPS_ROUTE_INFO.xls")
     out_file = "../../resource/cnap/BUPPS_ROUTE_INFO.sql"
     with open(out_file, "w", encoding="UTF-8") as f:
-        for index, row in data.iterrows():
+        for index, row in result.iterrows():
+            route_code = msg_type_to_route_code(str(row['报文编号']), str(row['SYSCODE']))
             sql = "INSERT INTO BUPPS_ROUTE_INFO (\"ROUTE_CODE\", \"ROUTE_NAME\", \"PAY_CHANNEL\", \"PAY_CHANNEL_NAME\", \"CHANNEL_TRANSCODE\", \"MSG_TYPE\", \"BUSI_MODE\", \"ROUTE_CHECK_FLAG\", \"ROUTE_PKG_FLAG\", \"SWITCH_FLAG\", \"EFFECTIVE_TIME1\", \"EFFECTIVE_TIME2\", \"EFFECTIVE_TIME3\", \"MAX_AMT\", \"MIN_AMT\", \"TIME_EFFECT_FLAG\", \"FEE_RULE\", \"CUSTOMER_FLAG\", \"CASH_FLAG\", \"CREATE_DATETIME\", \"UPDATE_DATETIME\", \"PRODUCT_RESERVE1\", \"PRODUCT_RESERVE2\", \"PRODUCT_RESERVE3\", \"RUNNING_STATUS\") VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '000', '1', '0', '0', '000001-092122', '092123-140000', '140001-235959', '0', '0', '2', '1', '1', '1', to_char(sysdate, 'yyyymmddhh24miss'), to_char(sysdate, 'yyyymmddhh24miss'), NULL, NULL, NULL, NULL);".format(
-                row['汇路编号'], row['报文名称'], row['通道编码'], row['通道编码'], "", row['报文编号'])
-            f.writelines(sql)
-            f.writelines("\n")
-
-
-def generate_route_service_rel(data):
-    """
-    根据数据集生成BUPPS_ROUTE_SERVICE_REL表SQL文件
-    :param data:
-    :return:
-    """
-    out_file = "../../resource/cnap/BUPPS_ROUTE_SERVICE_REL.sql"
-    with open(out_file, "w", encoding="UTF-8") as f:
-        for index, row in data.iterrows():
-            data_id = row['汇路编号'] + row['服务类型']
-            sql = "INSERT INTO BUPPS_ROUTE_SERVICE_REL (\"DATA_ID\", \"ROUTE_CODE\", \"ROUTE_NAME\", \"PAY_CHANNEL\", \"SERVICE_TYPE_CODE\", \"SERVICE_TYPE_NAME\", \"PRIORITY\", \"CREATE_DATETIME\", \"UPDATE_DATETIME\") VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '1', to_char(sysdate, 'yyyymmddhh24miss'), to_char(sysdate, 'yyyymmddhh24miss'));".format(
-                data_id, row['汇路编号'], row['报文名称'].rstrip(), row['通道编码'].upper(), row['服务类型'], row['报文名称'])
+                route_code, row['报文名称'], row['SYSCODE'], row['SYSCODE'], "", row['报文编号'])
             f.writelines(sql)
             f.writelines("\n")
 
@@ -68,30 +60,27 @@ def generate_service_busi_rel(data):
     out_file = "../../resource/cnap/BUPPS_SERVICE_BUSI_TYPE_REL.sql"
     with open(out_file, "w", encoding="UTF-8") as f:
         for index, row in data.iterrows():
-            sql = "INSERT INTO BUPPS_SERVICE_BUSI_TYPE_REL(\"DATA_ID\", \"BUSI_TYPE_CODE\", \"BUSI_KIND_CODE\", \"BUSI_TYPE_NAME\", \"BUSI_KIND_NAME\", \"SERVICE_TYPE_CODE\", \"SERVICE_TYPE_NAME\", \"CREATE_DATETIME\", \"UPDATE_DATETIME\") VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', to_char(sysdate,'yyyymmddhh24miss'), to_char(sysdate,'yyyymmddhh24miss');".format(
-                row['服务类型'], row['服务类型名称'])
+            data_id = str(row['服务类型']) + str(row['BIZTYPE']) + str(row['BIZDTLTYPEPURPOSE'])
+            sql = "INSERT INTO BUPPS_SERVICE_BUSI_TYPE_REL(\"DATA_ID\", \"BUSI_TYPE_CODE\", \"BUSI_KIND_CODE\", \"BUSI_TYPE_NAME\", \"BUSI_KIND_NAME\", \"SERVICE_TYPE_CODE\", \"SERVICE_TYPE_NAME\", \"CREATE_DATETIME\", \"UPDATE_DATETIME\") VALUES ('{}', '{}', '{}', '', '', '{}', '{}', to_char(sysdate,'yyyymmddhh24miss'), to_char(sysdate,'yyyymmddhh24miss');".format(
+                data_id, row['BIZTYPE'], row['BIZDTLTYPEPURPOSE'], row['服务类型'], row['服务类型名称'])
             f.writelines(sql)
             f.writelines("\n")
 
 
-def merge_busi_type(type1, type2):
-    if pd.isna(type1):
-        return type2
-    else:
-        return type1
-
-
 if __name__ == '__main__':
-    rel = get_all_cnap_msg_type(is_only_req=True)
-    rel['汇路编号'] = rel.apply(lambda row: msg_type_to_route_code(row['报文编号'], row['通道编码']), axis=1)
-    rel['服务类型'] = rel['报文编号'].apply(lambda x: msg_type_to_service_type(x))
-    rel['服务类型名称'] = rel['报文名称'].apply(lambda x: x.replace('报文', ''))
-    generate_route_info(rel)
-    print("生成BUPPS_ROUTE_INFO表")
-    generate_route_service_rel(rel)
-    print("生成BUPPS_ROUTE_SERVICE_REL表")
-    rel.to_excel("../../resource/cnap/BUPPS_ROUTE_SERVICE_REL.xls", sheet_name="BUPPS_ROUTE_SERVICE_REL")
-
-    service_type = rel.drop_duplicates('服务类型', keep="first", inplace=False)
-    generate_service_type(service_type)
-    print("生成BUPPS_SERVICE_TYPE_INFO表")
+    source = "../../resource/CCMSZDT0307.XML"
+    xtree = et.parse(source)
+    xroot = xtree.getroot()
+    df_cols = ['SYSCODE', 'MSGTYPE', 'BIZTYPE', 'BIZDTLTYPEPURPOSE']
+    rows = []
+    for node in xroot:
+        syscode = node.find("SYSCODE").text if node is not None else None
+        msgtype = node.find("MSGTYPE").text if node is not None else None
+        biztype = node.find("BIZTYPE").text if node is not None else None
+        bizdtltypepurpose = node.find("BIZDTLTYPEPURPOSE").text if node is not None else None
+        rows.append({"SYSCODE": syscode,
+                     "MSGTYPE": msgtype,
+                     "BIZTYPE": biztype,
+                     "BIZDTLTYPEPURPOSE": bizdtltypepurpose})
+    out_df = pd.DataFrame(rows, columns=df_cols)
+    generate_route_info(out_df.copy(deep=True))
